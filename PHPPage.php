82880870ff7beef816b33ce7650c401fc7368443
wax125 <wax125@gmail.com>
2014-09-24 10:36:50 +0200

    1

---
+++ PHPPage.php
@@ -0,0 +1,15 @@
+<?php
+    $test = "treść 1";
+print_r($test);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
